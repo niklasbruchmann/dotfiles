d5de6c6112ff41e3024c7f81b58a72d4e1a81d71
Niklas Bruchmann <18288175+niklasbruchmann@users.noreply.github.com>
2026-03-22 20:09:54 +0100

    chore: add rust analyser

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -607,7 +607,7 @@ require('lazy').setup({
                 -- clangd = {},
                 -- gopls = {},
                 -- pyright = {},
-                -- rust_analyzer = {},
+                rust_analyzer = {},
                 --
                 -- Some languages (like typescript) have entire language plugins that can be useful:
                 --    https://github.com/pmizio/typescript-tools.nvim
