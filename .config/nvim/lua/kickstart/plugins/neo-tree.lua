ceb171748ebb55bd4b3e8ed56765e98055b4d3e3
Niklas Bruchmann <18288175+niklasbruchmann@users.noreply.github.com>
2026-03-22 19:47:20 +0100

    chore: update fmt, update neotree keymap

--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -4,29 +4,31 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  ---@module 'neo-tree'
-  ---@type neotree.Config
-  opts = {
-    filesystem = {
-      filtered_items = {
-        hide_dotfiles = false,
-      },
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+        'MunifTanjim/nui.nvim',
     },
-  },
+    lazy = false,
+    keys = {
+        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+        { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    },
+    ---@module 'neo-tree'
+    ---@type neotree.Config
+    opts = {
+        filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+            },
+            window = {
+                mappings = {
+                    ['\\'] = 'close_window',
+                    ['<leader>e'] = 'close_window',
+                },
+            },
+        },
+    },
 }
